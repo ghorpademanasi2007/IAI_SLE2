f27c56c66615f09e5145998c7c60f2e9742a0fdd
ghorpademanasi2007 <ghorpademanasi2007@gmail.com>
2026-09-22 17:08:55 +0530

    Add BFS implementation with profiling support

---
+++ BFS.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+    order = []
+
+    while queue:
+        vertex = queue.popleft()
+        order.append(vertex)
+
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+    return order
+
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+if __name__ == '__main__':
+    start = 'A'
+    result = bfs(graph, start)
+    print('BFS traversal:', ' -> '.join(result))
